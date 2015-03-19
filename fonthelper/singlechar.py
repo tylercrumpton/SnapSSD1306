c899464a259842c3d48b976edb40bf64fce6d288
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-03-19 00:13:38 -0500

    Reads in image file from command line.

--- fonthelper/singlechar.py
+++ fonthelper/singlechar.py
@@ -1,7 +1,13 @@
-#import pillow
+from PIL import Image
 import argparse
+
+class CharacterImageHandler(object):
+    def __init__(self, filename):
+        self.im = Image.open(filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert a single character image to a font image for the SSD1306.')
     parser.add_argument('image', help='image filename')
     args = parser.parse_args()
+
+    cih = CharacterImageHandler(args.image)
