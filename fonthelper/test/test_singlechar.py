561dd6d93e8fa98e289ff076dd4cd8c7013f8336
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-03-19 22:31:33 -0500

    My unit tests were bad and I should feel bad.

--- fonthelper/test/test_singlechar.py
+++ fonthelper/test/test_singlechar.py
@@ -2,6 +2,7 @@
 """Unit tests for singlechar module"""
 import unittest
 import logging
+from mock import Mock, patch
 from os import path
 from fonthelper.singlechar import CharacterImageHandler
 
@@ -20,9 +21,10 @@ def teardown_module():
 class TestCharacterCheckImageSize(unittest.TestCase):
     """Verify CharacterImageHandler check_image_size function"""
 
-    def setUp(self):
+    @patch('PIL.Image.open')
+    def setUp(self, stub_open):
         self.longMessage = True  # Enable more verbose assertions
-
+        self.cih = CharacterImageHandler(None)
         logging.info("Starting Test: %s" % (self.shortDescription()))
 
     def tearDown(self):
@@ -37,16 +39,16 @@ class TestCharacterCheckImageSize(unittest.TestCase):
     ##########
     def test_image_size_correct(self):
         """Test that check_image_size returns True for 8x8"""
-        cih = CharacterImageHandler(path.join("fonthelper","test","percent.bmp"))
-        self.assertTrue(cih.check_image_size())
+        self.cih.im.size = (8,8)
+        self.assertTrue(self.cih.check_image_size())
     def test_image_size_small(self):
         """Test that check_image_size returns False for smaller than 8x8"""
-        cih = CharacterImageHandler(path.join("fonthelper","test","toosmall.bmp"))
-        self.assertFalse(cih.check_image_size())
+        self.cih.im.size = (4,4)
+        self.assertFalse(self.cih.check_image_size())
     def test_image_size_large(self):
         """Test that check_image_size returns False for larger than 8x8"""
-        cih = CharacterImageHandler(path.join("fonthelper","test","toobig.bmp"))
-        self.assertFalse(cih.check_image_size())
+        self.cih.im.size = (10,10)
+        self.assertFalse(self.cih.check_image_size())
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(msecs)03d %(levelname)-8s %(name)-8s %(message)s',
